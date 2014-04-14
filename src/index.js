cd3ca08e0ad6dad2318124b6c3beacec6fbb3d98
Gregers Rygg <gregersrygg@gmail.com>
2014-04-14 10:11:24 +0200

    Add support for reading params as URL-encoded JSON from the URL fragment

--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ var getAppender = require('./log/getAppender.js');
 var logger      = require('./log/logger.js');
 var eventListener = require('eventlistener');
 var childrenSize  = require('./childrensize.js');
+var extend        = require('util-extend');
+
+function readParams () {
+    // Params are passed as a JSON in the url fragment by default. IE has a limit for URLs longer than 2083 bytes, so
+    // fallback to iframe.name if there is no url fragment.
+    // We can't only rely on iframe.name because WebViews from native apps don't use an iframe, and it's currently no
+    // easy way to define window.name synchronously before the document is loaded on iOS.
+    var urlFragment = decodeURIComponent(document.location.hash.substring(1));
+    return JSON.parse(urlFragment || window.name);
+}
 
 var bootStrap = function () {
     var gardr = global.gardr || {};
     if (!global.gardr) { global.gardr = gardr; }
 
-    gardr.params = gardr.params || JSON.parse(window.name);
+    gardr.params = extend(gardr.params || {}, readParams());
     gardr.id = gardr.params.id;
     gardr.log = logger.create(gardr.id, gardr.params.loglevel, getAppender(gardr.params.logto));
 
