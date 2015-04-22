8387ed1ffb9ce0dfcb1bc68a5b631d89bbbf2a47
sveisvei <sveinung.rosaker@gmail.com>
2015-04-22 11:01:29 +0200

    using es5-shim instead

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,6 @@ module.exports = function(config) {
 
         files: [
             'node_modules/ie8/build/ie8.max.js',
-            'test/lib/Function-polyfill.js'
             'test/**/*.js'
         ],
 
